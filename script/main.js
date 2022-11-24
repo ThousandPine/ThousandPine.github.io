1cbd74741f2e3a8277658c746b819945d8e63ead
Pine <2277367263@qq.com>
2022-11-24 19:52:01 +0800

    删除了额外图片和点击切换功能 为index添加了html标识头

--- script/main.js
+++ script/main.js
@@ -2,14 +2,14 @@ let myHeading = document.querySelector("h1");
 let myButton = document.querySelector('button');
 // myHeading.textContent = "Hello =v=!";
 
-let myImage = document.querySelector('img');
-myImage.onclick = () => {
-    let src = myImage.getAttribute('src');
-    if (src === "images/ev07222.jpg")
-        myImage.setAttribute('src', 'images/QQ图片20220623105031.jpg');
-    else
-        myImage.setAttribute('src', 'images/ev07222.jpg');
-}
+// let myImage = document.querySelector('img');
+// myImage.onclick = () => {
+//     let src = myImage.getAttribute('src');
+//     if (src === "images/ev07222.jpg")
+//         myImage.setAttribute('src', 'images/QQ图片20220623105031.jpg');
+//     else
+//         myImage.setAttribute('src', 'images/ev07222.jpg');
+// }
 
 if (!localStorage.getItem('name')) {
     setUserName();
@@ -23,6 +23,7 @@ myButton.onclick = () => { setUserName(); }
 function setUserName() {
     let myName = prompt('What\'s your name?');
     localStorage.setItem('name', myName);
+    
     myHeading.textContent = 'Hello ' + myName + '!';
 }
 
